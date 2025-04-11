26c91463a4e279a0a85be9feab147eb646ee779b
“Allan <elgarallan@gmail.com>
2025-04-11 10:13:17 +0800

    initial commit

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,48 @@
+class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_category, only: [ :edit, :update, :destroy ]
+
+  def index
+    @categories = current_user.categories
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = current_user.categories.build(category_params)
+    if @category.save
+      redirect_to root_path, alert: "Category created successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @category.update(category_params)
+      redirect_to root_path, notice: "Category updated successfully!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to root_path, notice: "Category deleted successfully!"
+  end
+
+private
+
+  def category_params
+    params.require(:category).permit(:title)
+  end
+
+  def set_category
+    @category = current_user.categories.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path, alert: "Category not found."
+  end
+end
---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_category
+
+  def new
+    @task = @category.tasks.build
+  end
+
+  def create
+    @task = @category.tasks.build(task_params)
+    if @task.save
+      redirect_to root_path, notice: "Task added successfully!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @task = @category.tasks.find(params[:id])
+  end
+
+  def update
+    @task = @category.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to root_path, notice: "Task updated successfully!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = @category.tasks.find(params[:id])
+    @task.destroy
+    redirect_to root_path, notice: "Task deleted successfully!"
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def task_params
+    params.require(:task).permit(:name, :due_date)
+  end
+end
